278dcda672e0498de2615c00a083e00586a496a2
KDH_Notebook <destiny1017@nate.com>
2021-09-08 21:30:43 +0900

    업비트 자동매매 개발

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pyupbit
 import requests
 import json
+import time
 from flask import make_response
 
-access = "aXKHw5tsr3uGwvZQ5bDq2qo5A0JPPTJUNkjj2CWD"
-secret = "kt6gQfE1dJqenmte2DmXDtzE6DAoz9IPmYx9InMK"
+upbit_keys = open("C:/upbit/access_key", "r", encoding="utf8")
+
+access = upbit_keys.readline().rstrip('\n')
+secret = upbit_keys.readline()
 upbit = pyupbit.Upbit(access, secret)
 
 # 계좌 보유 금액
@@ -14,25 +17,54 @@ print(upbit.get_balance("KRW"))
 print("=======================================================")
 
 # 종목 코드 정보
-url = "https://api.upbit.com/v1/market/all"
-
-querystring = {"isDetails": "false"}
-headers = {"Accept": "application/json"}
-
-response = requests.request("GET", url, headers=headers, params=querystring).json()
+# url = "https://api.upbit.com/v1/market/all"
+#
+# querystring = {"isDetails": "false"}
+# headers = {"Accept": "application/json"}
+#
+# response = requests.request("GET", url, headers=headers, params=querystring).json()
 # response.raise_for_status()
 # response.encoding = 'utf8'
 
 # print(json.loads(response.text))
 
-if len(response) > 0:
-    f = open("files/items_code.txt", "a", encoding="utf8")
-    for item in response:
-        market = item["market"]
-        korean_name = item["korean_name"]
-        english_name = item["english_name"]
+# 종목 정보 파일 저장
+# if len(response) > 0:
+#     f = open("files/items_code.txt", "a", encoding="utf8")
+#     for item in response:
+#         market = item["market"]
+#         korean_name = item["korean_name"]
+#         english_name = item["english_name"]
+#
+#         f.write("%s\t%s\t%s\n" % (market, korean_name, english_name))
+#         # print("%s\t%s\t%s\n" % (market, korean_name, english_name))
+#
+#     f.close()
 
-        f.write("%s\t%s\t%s\n" % (market, korean_name, english_name))
-        # print("%s\t%s\t%s\n" % (market, korean_name, english_name))
+# minute1 = pyupbit.get_ohlcv("KRW-SSX", "minute1")
+# print(minute1)
+# print(type(minute1), minute1.shape)
+# print(minute1.index[-1:])
 
-    f.close()
+# for min in minute1.values.tolist():
+#     print(min)
+# while True:
+#     minute1 = pyupbit.get_ohlcv("KRW-SSX", "minute1").values.tolist()
+#     print(minute1[-1:])
+#     time.sleep(1)
+
+last_min = None
+while True:
+    minute1 = pyupbit.get_ohlcv("KRW-SSX", "minute1", 1)
+    print(minute1)
+    current_min = minute1.index
+    if last_min != current_min:
+        print("changed min...")
+
+    print(current_min)
+    last_min = current_min
+    time.sleep(1)
+
+# >> 1분마다 반복 알고리즘
+# 1. 최근 30분간의 1분 평균 거래량 산출
+# 2. 마지막의 1분봉 거래량이 1에서 산출한 거래량의 4배 이상이고 양봉인 종목 검색
