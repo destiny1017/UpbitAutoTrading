d667aaa00ffd258216ad6b2b7727f496f68e2402
KDH_Notebook <destiny1017@nate.com>
2021-09-14 20:56:10 +0900

    업비트 자동매매_Oracle연동

---
+++ DB_conn/DB_conn_test.py
@@ -0,0 +1,38 @@
+import cx_Oracle as ora
+import os
+
+
+# 쿼리 결과를 dictionary형태로 받아오기위한 함수 오버라이딩
+def makeDictFactory(cursor):
+    columnNames = [d[0] for d in cursor.description]
+
+    def createRow(*args):
+        return dict(zip(columnNames, args))
+
+    return createRow
+
+
+# 인코딩 설정
+os.putenv('NLS_LANG', '.UTF8')
+
+# DB연결
+conn = ora.connect('c##upbitpy', '1234', '172.30.1.33:1522/XE')
+
+# 커서 할당
+cursor = conn.cursor()
+
+# 쿼리 실행
+cursor.execute("""
+    select *
+    from test
+""")
+
+# 딕셔너리변환 함수 오버라이딩
+cursor.rowfactory = makeDictFactory(cursor)
+
+# 오버라이딩한 함수로 쿼리 결과 받아오기
+rows = cursor.fetchall()
+
+# 결과 출력
+for row in rows:
+    print(row)
