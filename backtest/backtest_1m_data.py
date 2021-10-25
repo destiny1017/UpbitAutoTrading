53dff4349d38b37e149b11389f0a2fd0007e92e0
KDH_Notebook <destiny1017@nate.com>
2021-10-25 23:56:39 +0900

    upbit자동매매 api

--- backtest/backtest_1m_data.py
+++ backtest/backtest_1m_data.py
@@ -27,20 +27,31 @@ cursor.rowfactory = makeDictFactory(cursor)
 rows = cursor.fetchall()
 arr = []
 avg_vol = 0
+avg_vol_min = []
 
 for i, val in enumerate(rows):
     # 최근 60분간의 데이터로 평균 거래량 구하기
     if i <= 60:
         arr.append(val['VOLUME'])
     else:
-        del arr[0]
+        del arr[0]  # 60분 평균값을 구해야하므로 가장 앞의 데이터를 하나씩 지워서 길이 60을 유지
         np_arr = np.array(arr)
-        avg_vol = np.mean(np_arr)
+        avg_vol = int(np.mean(np_arr))
+        # 5분 전까자의 60분 평균거래량을 저장하는 배열에 추가
+        avg_vol_min.append(avg_vol)
+        if len(avg_vol_min) > 5:
+            del avg_vol_min[0]
+
         arr.append(val['VOLUME'])
 
         # 최근 60분 평균거래량의 10배가 넘는 거래량이 터지는 지점 포착
-        if val['VOLUME'] > avg_vol*10:
-            print("TIME_IDX: %s, val['VOLUME'] : %d, avg_vol: %d" % (val['TIME_IDX'], val['VOLUME'], avg_vol))
+        # if val['VOLUME'] > avg_vol*10:
+        #     print("TIME_IDX: %s, val['VOLUME'] : %d, avg_vol: %d" % (val['TIME_IDX'], val['VOLUME'], avg_vol))
+
+        # -5분지점부터 60분간 평균거래량의 10배가 넘는 거래량이 터지는 지점 포착
+        if val['VOLUME'] > avg_vol_min[0]*10:
+            print("-5min -> TIME_IDX: %s, val['VOLUME'] : %d, avg_vol: %d" % (val['TIME_IDX'], val['VOLUME'], avg_vol))
+            print(avg_vol_min)
 
 
     # print("time: %s, avg_vol: %d" % (val['TIME_IDX'], avg_vol))
